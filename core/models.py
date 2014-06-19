07823fab9b49b361e24a7c66557668055be1da99
kakulukia <andy@freilandkiwis.de>
2014-06-19 20:10:39 +0200

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,25 +1,35 @@
 # coding=utf-8
 from django.db import models
-
+from django.db.models.query import QuerySet
+from django.utils.datetime_safe import datetime
 
 # basic model stuff
 ##########################################
-from django.db.models import Sum
-from django.forms import forms
-from django.utils.datetime_safe import datetime
+class DataQuerySet(QuerySet):
+    def delete(self):
+        self.update(active=False, deleted=datetime.now())
+
+
+class AllObjectsQuerySet(QuerySet):
+    def undelete(self):
+        self.update(active=True, deleted=None)
 
 
 class DataManager(models.Manager):
+    use_for_related_fields = True
 
     def get_query_set(self):
-        qs = super(DataManager, self).get_query_set()
+        qs = DataQuerySet(self.model, using=self._db)
         return qs.filter(active=True)
-        
+
     def delete(self):
-        self.update(active=False)
+        self.update(active=False, deleted=datetime.now())
 
 
 class AllObjectsManager(models.Manager):
+
+    def get_query_set(self):
+        return DataQuerySet(self.model, using=self._db)
 
     def deleted(self):
         return self.filter(active=False)
@@ -28,11 +38,11 @@ class AllObjectsManager(models.Manager):
 # base model with useful stuff
 class BaseModel(models.Model):
 
-    created = models.DateTimeField(auto_now_add=True, editable=False)
-    modified = models.DateTimeField(auto_now=True, editable=False)
+    created = models.DateTimeField(auto_now_add=True, editable=False, db_index=True)
+    modified = models.DateTimeField(auto_now=True, editable=False, db_index=True)
     deleted = models.DateTimeField(editable=False, null=True)
 
-    active = models.BooleanField(default=True, editable=False)
+    active = models.BooleanField(default=True, editable=False, db_index=True)
 
     # access only active data objects
     data = DataManager()
@@ -48,8 +58,11 @@ class BaseModel(models.Model):
         self.active = False
         self.save()
 
-    def save(self, force_insert=False, force_update=False, using=None,
-             update_fields=None):
+    def undelete(self, using=None):
+        self.active = True
+        self.save()
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
 
         if self.active:
             self.deleted = None
@@ -64,8 +77,8 @@ class BaseModel(models.Model):
             return self.__class__.objects.get(id=self.id)
 
 
-class NamedModel(models.Model):
-    name = models.CharField('Name', max_length=150)
+class NamedModel(BaseModel):
+    name = models.CharField('Name', max_length=150, db_index=True)
 
     class Meta:
         ordering = ['name']
@@ -73,4 +86,3 @@ class NamedModel(models.Model):
 
     def __unicode__(self):
         return self.name
-
